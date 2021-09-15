417eed2ba2bbf94f14488fe2625216f42cc5fa4a
Adam Purcell <apurcell88@gmail.com>
2021-09-15 14:40:35 -0500

    Added a bunch of functions/logic to get the DOM interactive

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,40 @@
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissor = document.querySelector('#scissor');
+const buttons = document.querySelectorAll('.btn');
+const results = document.querySelector('#results-container');
+const summaryContainer = document.querySelector('#summary-container');
+
 let playerScore = 0;
 let computerScore = 0;
+
+let playerScoreEl = document.createElement('h2');
+let computerScoreEl = document.createElement('h2');
+
+rock.addEventListener('click', (e) => {
+    checkGameOver()
+
+    if (playerScore !== 5 || computerScore !== 5) {
+        playRound('rock', computerPlay());
+    }
+});
+
+paper.addEventListener('click', (e) => {
+    checkGameOver();
+
+    if (playerScore !== 5 || computerScore !== 5) {
+        playRound('paper', computerPlay());
+    }
+    
+});
+
+scissor.addEventListener('click', (e) => {
+    checkGameOver();
+    
+    if (playerScore !== 5 || computerScore !== 5) {
+        playRound('scissor', computerPlay());
+    }
+});
 
 function computerPlay() {
     let moves = ['rock', 'paper', 'scissor'];
@@ -9,36 +44,92 @@ function computerPlay() {
     return play;
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissor') {
+function createElement(el, text) {
+    const element = document.createElement(el);
+    element.textContent = text;
+
+    return element;
+}
+
+function updatePlayerScore() {
+    if (playerScore < 5) {
         playerScore++;
-        console.log('You win! Rock beats Scissor');
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
-        playerScore++;
-        console.log('You win! Paper beats Rock');
-    } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'paper') {
-        playerScore++;
-        console.log('You win! Scissor beats paper');
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
-        console.log('It\'s a draw! Rock can\'t beat Rock');
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') {
-        console.log('It\'s a draw! Paper can\'t beat Paper');
-    } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'scissor') {
-        console.log('It\'s a draw! Scissor can\'t beat Scissor');
-    } else {
+        summaryContainer.appendChild(createElement('h1', `${playerScore} - ${computerScore}`));
+    } else if (playerScore === 5) {
+        summaryContainer.appendChild(createElement('h1', `You Win!`));
+    }
+}
+
+let yay = document.createElement('h2');
+// summaryContainer.appendChild(createElement('h1', `${playerScore} - ${computerScore}`));
+
+function updateComputerScore() {
+    if (computerScore < 5) {
         computerScore++;
-        console.log('You lose!');
+        yay.textContent = `${playerScore} - ${computerScore}`;
+        summaryContainer.appendChild(yay);
+    } else if (computerScore === 5) {
+        yay.textContent = 'You Lose!'
+        summaryContainer.appendChild(yay);
+    }
+    
+}
+
+function makeDraw() {
+    summaryContainer.appendChild(createElement('h1', `${playerScore} - ${computerScore}`));
+}
+
+function checkGameOver() {
+    if (playerScore === 5 && computerScore !== 5) {
+        buttons.forEach((button) => {
+            button.disabled = true;
+        });
+    } else if (playerScore !== 5 && computerScore === 5) {
+        buttons.forEach((button) => {
+            button.disabled = true;
+        });
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerScore !== 5 && computerScore !== 5) {
+        results.innerHTML = '';
+        summaryContainer.innerHTML = '';
+
+        if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissor') {
+            updatePlayerScore();
+            results.appendChild(createElement('h2', 'You win! Rock beats Scissor'));
+        } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+            updatePlayerScore();
+            results.appendChild(createElement('h2', 'You win! Paper beats Rock'))
+        } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'paper') {
+            updatePlayerScore();
+            results.appendChild(createElement('h2', 'You win! Scissor beats paper'));
+        } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
+            makeDraw();
+            results.appendChild(createElement('h2', 'It\'s a draw! Rock can\'t beat Rock'));
+        } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') {
+            makeDraw();
+            results.appendChild(createElement('h2', 'It\'s a draw! Paper can\'t beat Paper'));
+        } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'scissor') {
+            makeDraw();
+            results.appendChild(createElement('h2', 'It\'s a draw! Scissor can\'t beat Scissor'));
+        } else {
+            updateComputerScore();
+            results.appendChild(createElement('h2', 'You lose!'));
+        }
     }
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('What is your move? ');
-        playRound(playerSelection, computerPlay());
-    }
+    // for (let i = 0; i < 5; i++) {
+    //     let playerSelection = prompt('What is your move? ');
+    //     playRound(playerSelection, computerPlay());
+    // }
     
-    if (playerScore > computerScore) {
-        console.log('You win the game!');
+    if (playerScore !== 5) {
+        // console.log('You win the game!');
     } else if (playerScore === computerScore) {
         console.log('It\'s a draw.');
     } else {
@@ -46,4 +137,4 @@ function game() {
     }
 }
 
-game();
+// game();
